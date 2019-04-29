647194301d79f184ad9eba1e5eed3ac4f74f622d
afeiship <1290657123@qq.com>
2019-04-29 11:40:02 +0800

    add: intial

---
+++ generators/app/index.js
@@ -0,0 +1,74 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const glob = require('glob');
+const { resolve } = require('path');
+const remote = require('yeoman-remote');
+const yoHelper = require('yeoman-generator-helper');
+const replace = require('replace-in-file');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(
+        `Welcome to the stunning ${chalk.red(
+          'generator-server'
+        )} generator!`
+      )
+    );
+
+    const prompts = [
+      {
+        type: 'input',
+        name: 'project_name',
+        message: 'Your project_name (eg: like this `react-button` )?',
+        default: yoHelper.discoverRoot
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Your description?'
+      }
+    ];
+
+    return this.prompt(prompts).then(
+      function(props) {
+        // To access props later use this.props.someAnswer;
+        this.props = props;
+        yoHelper.rewriteProps(props);
+      }.bind(this)
+    );
+  }
+
+  writing() {
+    const done = this.async();
+    remote(
+      'afeiship',
+      'boilerplate-server',
+      function(err, cachePath) {
+        // copy files:
+        this.fs.copy(
+          glob.sync(resolve(cachePath, '{**,.*}')),
+          this.destinationPath()
+        );
+        done();
+      }.bind(this)
+    );
+  }
+
+  end() {
+    const { project_name, description } = this.props;
+    const files = glob.sync(resolve(this.destinationPath(), '{**,.*}'));
+
+    replace.sync({
+      files,
+      from: [
+        /boilerplate-server-description/g,
+        /boilerplate-server/g,
+      ],
+      to: [description,  project_name]
+    });
+  }
+};
